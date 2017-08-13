23b915fbf1842a26e8a9938c6ba1760322ead321
tdostilio <tdostilio@gmail.com>
2017-08-13 17:58:04 -0400

    got pug button to load

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+
+const pug = require('pug');
+const app = require("express")();
+const stripe = require("stripe")(keySecret);
+
+
+app.get("/", (req, res) =>
+  res.render("index.pug", {keyPublishable}));
+
+app.post("/charge", (req, res) => {
+  let amount = 500;
+
+  stripe.customers.create({
+     email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer =>
+    stripe.charges.create({
+      amount,
+      description: "Sample Charge",
+         currency: "usd",
+         customer: customer.id
+    }))
+  .then(charge => res.render("charge.pug"));
+});
+
+app.listen(4567);
